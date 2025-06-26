d11eb24ab3b1f776de29094a58c9589df71e7744
RamsesCode <RamsesSanchez25@gmail.com>
2025-06-26 15:15:53 -0400

    some functions working

--- api/tasks.js
+++ api/tasks.js
@@ -4,16 +4,36 @@ const { Task } = require("../dummy-database");
 
 // GET all tasks
 router.get("/", (req, res) => {
-  // Replace this with your code!
-  res.status(501).send("Not implemented");
+  const allTasks = Task.findAll()
+  res.send(allTasks);
 });
 
 // GET a single task by id
+router.get("/:id", (req, res)=>{
+  const id = Number(req.params.id);
+  const singleTask = Task.findByPk(id);
+  res.send(singleTask);
+})
 
 // Patch a task by id
 
 // Delete a task by id
 
+router.delete("/:id", (req, res) => {
+  const id = Number(req.params.id);
+  try {
+    Task.delete(id);
+    res.send({ success: true });
+  } catch (error) {
+    res.status(404).send({ error: "Task not found" });
+  }
+});
+
 // Create a new task
+
+router.post("/", (req, res) => {
+  const newTask = Task.create(req.body);
+  res.send(newTask); 
+});
 
 module.exports = router;
